b2bfabd739da0dd4975ba2124d3c106b97b8fba2
Jairo Ushiña <jago86@gmail.com>
2021-12-12 22:09:38 -0500

    clase 2.5

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
     extend: {
       spacing: {
         'quarter': '25%',
+      },
+      fontFamily: {
+        'sans': "'Work Sans', sans-serif",
+        'cascadia': '"CascadiaCode"',
+      },
+      colors: {
+        'do-blue-dark': '#080c2d',
+        'do-blue-medium': 'rgb(20, 86, 255)',
+        'do-blue-light': 'rgb(0, 105, 255)',
       }
     },
   },
