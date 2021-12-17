6da316cdf863f755e8688b7a4f906211055825ea
Jairo Ushiña <jago86@gmail.com>
2021-12-17 18:29:04 -0500

    Clase 3.7

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,9 @@ module.exports = {
         'input': '0 5px 1px 0 rgb(0, 0, 0, 0.1)',
         'input-focus': '0 2px 1px 0 rgb(0, 0, 0, 0.1)',
       },
+      width: {
+        '88': '22rem',
+      }
     },
   },
   plugins: [],
