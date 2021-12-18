3a212bdbfa2294aa68f4afe75643af1878b20013
Jairo Ushiña <jago86@gmail.com>
2021-12-18 03:24:41 -0500

    Clase 12

--- js/app.js
+++ js/app.js
@@ -9,6 +9,38 @@ Vue.component('link-cascadia', {
     `
 })
 
+Vue.component('panel-blog', {
+    template: `
+        <div class="w-1/4 bg-white shadow-lg rounded-lg">
+            <div>
+                <slot name="image"></slot>
+            </div>
+
+            <div class="py-6 px-6">
+                <div class="uppercase text-sky-500 tracking-widest">
+                    <slot name="type"></slot>
+                </div>
+                <div class="font-medium text-lg my-5">
+                    <slot name="title"></slot>
+                </div>
+                <div class="flex text-gray-500 text-sm">
+                    <div class="w-1/2">
+                        <slot name="date"></slot>
+                    </div>
+                    <div class="flex items-center w-1/2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
+                        <slot name="visits"></slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+})
+
 new Vue({
     el: '#app'
 })
