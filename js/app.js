d45029dec525e640be44b778785b036cae6665cc
Jairo Ushiña <jago86@gmail.com>
2021-12-23 19:13:33 -0500

    Clase 6.3

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ Vue.component('link-cascadia', {
     template: `
         <a class="group flex items-center text-do-blue-light hover:text-do-blue-medium font-cascadia text-lg font-bold" href="">
             <slot></slot>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 transition-all group-hover:ml-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 transition-all duration-300 group-hover:ml-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
             </svg>
         </a>
@@ -11,7 +11,7 @@ Vue.component('link-cascadia', {
 
 Vue.component('panel-blog', {
     template: `
-        <div class="flex flex-col bg-white transition-shadow shadow-lg hover:shadow-2xl rounded-lg">
+        <div class="flex flex-col bg-white transition-shadow duration-300 shadow-lg hover:shadow-2xl rounded-lg">
             <div>
                 <slot name="image"></slot>
             </div>
