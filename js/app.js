143433398ccb6c97219b3bd95560143550e86fa8
Jairo Ushiña <jago86@gmail.com>
2021-12-26 17:41:02 -0500

    Clase 6.7

--- js/app.js
+++ js/app.js
@@ -48,37 +48,37 @@ new Vue({
         theme: 'light',
     },
 
-    // mounted() {
-    //     this.theme = localStorage.theme ?? 'system'
-    //     this.applyTheme()
-    // },
+    mounted() {
+        this.theme = localStorage.theme ?? 'system'
+        this.applyTheme()
+    },
 
-    // methods: {
-    //     setDarkMode() {
-    //         localStorage.theme = 'dark'
-    //         document.getElementById('app').classList.add('dark')
-    //         this.theme = 'dark'
-    //         this.applyTheme()
-    //     },
-    //     setLightMode() {
-    //         localStorage.theme = 'light'
-    //         document.getElementById('app').classList.remove('dark')
-    //         this.theme = 'light'
-    //         this.applyTheme()
-    //     },
-    //     setSystemMode() {
-    //         localStorage.removeItem('theme')
-    //         document.getElementById('app').classList.remove('dark')
-    //         this.theme = 'system'
-    //         this.applyTheme()
-    //     },
+    methods: {
+        setDarkMode() {
+            localStorage.theme = 'dark'
+            document.getElementById('app').classList.add('dark')
+            this.theme = 'dark'
+            this.applyTheme()
+        },
+        setLightMode() {
+            localStorage.theme = 'light'
+            document.getElementById('app').classList.remove('dark')
+            this.theme = 'light'
+            this.applyTheme()
+        },
+        setSystemMode() {
+            localStorage.removeItem('theme')
+            document.getElementById('app').classList.remove('dark')
+            this.theme = 'system'
+            this.applyTheme()
+        },
 
-    //     applyTheme() {
-    //         if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    //             document.documentElement.classList.add('dark')
-    //         } else {
-    //             document.documentElement.classList.remove('dark')
-    //         }
-    //     }
-    // }
+        applyTheme() {
+            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark')
+            } else {
+                document.documentElement.classList.remove('dark')
+            }
+        }
+    }
 })
